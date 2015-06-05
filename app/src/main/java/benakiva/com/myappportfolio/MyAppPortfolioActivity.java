d4040d4e6efd5ec961af578e3604cce0bb7f02af
ibenakiva <isaac.ben-akiva@ubimobitech.com>
2015-06-05 15:26:12 +0100

    Implemented some awesome feedback I've received from Google/Udacity.

--- app/src/main/java/benakiva/com/myappportfolio/MyAppPortfolioActivity.java
+++ app/src/main/java/benakiva/com/myappportfolio/MyAppPortfolioActivity.java
@@ -1,6 +1,7 @@
 package benakiva.com.myappportfolio;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,8 +11,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 
-public class MyAppPortfolio extends Activity implements View.OnClickListener {
-    private Resources mRes;
+public class MyAppPortfolioActivity extends Activity {
     private Button mSpotify;
     private Button mScores;
     private Button mLibrary;
@@ -25,24 +25,16 @@ public class MyAppPortfolio extends Activity implements View.OnClickListener {
         setContentView(R.layout.activity_my_app_portfolio);
 
         mSpotify = (Button) findViewById(R.id.spotify_streamer);
-        mSpotify.setOnClickListener(this);
 
         mScores = (Button)findViewById(R.id.scores);
-        mScores.setOnClickListener(this);
 
         mLibrary = (Button)findViewById(R.id.library);
-        mLibrary.setOnClickListener(this);
 
         mBuildBigger = (Button)findViewById(R.id.build);
-        mBuildBigger.setOnClickListener(this);
 
         mXYZReader = (Button)findViewById(R.id.xyz_reader);
-        mXYZReader.setOnClickListener(this);
 
         mCapstone = (Button)findViewById(R.id.capstone);
-        mCapstone.setOnClickListener(this);
-
-        mRes = getResources();
     }
 
     @Override
@@ -67,41 +59,12 @@ public class MyAppPortfolio extends Activity implements View.OnClickListener {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * Called when a view has been clicked.
-     *
-     * @param v The view that was clicked.
-     */
-    @Override
-    public void onClick(View v) {
-        int id = v.getId();
+    private void launchToast(View view) {
+        Button button = (Button)view;
 
-        switch (id) {
-            case R.id.spotify_streamer:
-                launchToast(mRes.getString(R.string.spotify));
-                break;
-            case R.id.scores:
-                launchToast(mRes.getString(R.string.scores_app));
-                break;
-            case R.id.library:
-                launchToast(mRes.getString(R.string.library_app));
-                break;
-            case R.id.build:
-                launchToast(mRes.getString(R.string.build_bigger));
-                break;
-            case R.id.xyz_reader:
-                launchToast(mRes.getString(R.string.xyz_reader));
-                break;
-            case R.id.capstone:
-                launchToast(mRes.getString(R.string.capstone_app));
-                break;
-            default:
-                return;
-        }
-    }
+        String buttonText = (String) button.getText();
+        CharSequence text = getString(R.string.open_app) + buttonText;
 
-    private void launchToast(final String app) {
-        Toast.makeText(getApplicationContext(), mRes.getString(R.string.toast_msg, app),
-                Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
     }
 }
